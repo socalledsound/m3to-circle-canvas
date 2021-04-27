08904a3af340e4f03700e86373986208fd84269c
Chris Kubick <socalledsound@gmail.com>
2021-04-26 21:57:25 -0700

    add normalize.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import RootProvider from './RootProvider'
 import App from './App'
+import 'normalize.css'
+import './index.css'
 
 ReactDOM.render(
         <RootProvider>
